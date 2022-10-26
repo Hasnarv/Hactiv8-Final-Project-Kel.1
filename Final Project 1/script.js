6283487ccffdf0a2ca2eb2f02ec2347bb19d0be5
Hasnarv <72425456+Hasnarv@users.noreply.github.com>
2022-10-26 20:53:08 +0700

    add dashboard

--- Final Project 1/script.js
+++ Final Project 1/script.js
@@ -2,6 +2,9 @@ function Redirect() {
   window.location = "courses.html";
 }
 
+let nama = localStorage.getItem('name');
+document.getElementById('name').innerHTML = nama;
+
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
