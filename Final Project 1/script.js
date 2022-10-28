411ae1356a0429bb44c0bf9bb86c0cf81b98b71c
Hasnarv <72425456+Hasnarv@users.noreply.github.com>
2022-10-28 14:42:03 +0700

    new edit

--- Final Project 1/script.js
+++ Final Project 1/script.js
@@ -4,10 +4,3 @@ function Redirect() {
 
 let nama = localStorage.getItem('nama');
 document.getElementById('nama').innerHTML = nama;
-
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
