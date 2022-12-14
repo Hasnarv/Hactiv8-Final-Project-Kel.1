55a786ca72833225b69fe55abceb457515dab5b7
Hasnarv <72425456+Hasnarv@users.noreply.github.com>
2022-12-14 08:31:21 +0700

    progress fp4

--- Final Project 4/script.js
+++ Final Project 4/script.js
@@ -1,36 +1,53 @@
-document.getElementById('buttonClick').addEventListener('click', async () => {
-    // mengambil nilai yang diinputkan user di id inputUser ke variabel userInput
-    const userInput = document.getElementById('inputUser').value
-    // mengubah nilai dari id country dengan nilai yang diinputkan user
-    document.querySelector('#country').innerHTML = userInput 
+// const searchButton = document.querySelector('#button-addon2');
+// searchButton.addEventListener('click', async () => {
+//     // // mengambil nilai yang diinputkan user di id inputUser ke variabel userInput
+//     // const userInput = document.getElementById('inputUser').value
+//     // // mengubah nilai dari id country dengan nilai yang diinputkan user
+//     // document.querySelector('#city').innerHTML = userInput 
 
-    // mengambil data dari API
-    const options = {
-        method: 'GET',
-	    headers: {
-		'X-RapidAPI-Key':'3e8e82d6f8mshe3fe81fb48532bcp10b092jsnc3df82a20718',
-	}
-    };
-    const response = await fetch(`https://covid-193.p.rapidapi.com/history?country=${userInput}`, options)
-    const dataJson = await response.json()
+    
+//     const inputKeyword = document.querySelector('.input-keyword');
 
-    if (dataJson.results != 0) {
-        // jika negara yang diinputkan user ada di data API
-        const data = dataJson.response[0]
-        document.querySelector('#active').innerText = data.cases.active
-        document.querySelector('#newCases').innerText = data.cases.new
-        document.querySelector('#recoveredCases').innerText = data.cases.recovered
-        document.querySelector('#totalCases').innerText = data.cases.total
-        document.querySelector('#totalDeath').innerText =data.deaths.total
-        document.querySelector('#totalTest').innerText = data.tests.total
-    }else {
-        // jika negara yang diinputkan user tidak ada di data API
-        alert("No Data Covid-19 in " + userInput)
-        document.querySelector('#active').innerText = 0
-        document.querySelector('#newCases').innerText = 0
-        document.querySelector('#recoveredCases').innerText = 0
-        document.querySelector('#totalCases').innerText = 0
-        document.querySelector('#totalDeath').innerText = 0
-        document.querySelector('#totalTest').innerText = 0
-    }
+//     // mengambil data dari API
+
+//     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputKeyword}&appid=1fe5f03e8b679377cbc41601289edfdd&units=metric`)
+//     const dataJson = await response.json()
+
+//     if (dataJson.results != 0) {
+//         // jika negara yang diinputkan user ada di data API
+//         const data = dataJson.response[0]
+//         document.querySelector('#condition').innerText = data.weather.description
+//         document.querySelector('#temperature').innerText = data.main.temp
+//         document.querySelector('#windSpeed').innerText = data.wind.speed
+//     }else {
+//         // jika negara yang diinputkan user tidak ada di data API
+//         alert("No Data Covid-19 in " + userInput)
+//         document.querySelector('#temperature').innerText = 0
+//         document.querySelector('#windSpeed').innerText = 0
+//     }
+// })
+
+
+
+
+const searchButton = document.querySelector('#button-addon2');
+const inputKeyword = document.querySelector('.input-keyword');
+document.body.style.backgroundImage = 'url(img/sky.jpg)'
+
+searchButton.addEventListener('click', function() {
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputKeyword.value+"&appid=1fe5f03e8b679377cbc41601289edfdd&units=metric")
+        .then(response => response.json())
+        .then(response => {
+            let result = document.querySelector('.result')
+
+            result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
+                                <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
+                                <p style="margin-bottom: 17px;">Temperature from ${response.main.temp_min}°С to ${response.main.temp_max}°С</p>
+                                <h5>Wind Speed : ${response.wind.speed} m/s</h5>
+                                <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
+                                <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>` 
+        })
+    inputKeyword.value = null;
+
 })
